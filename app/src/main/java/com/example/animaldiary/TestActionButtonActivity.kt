387faf2d681be9d5710d8e31a7eabc513552790a
seungjae708 <choesj365@gmail.com>
2025-08-11 18:57:27 +0900

    :sparkles: [FEAT] (#32) Action Button 테스트 코드 구현

---
+++ app/src/main/java/com/example/animaldiary/TestActionButtonActivity.kt
@@ -0,0 +1,11 @@
+package com.example.animaldiary
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TestActionButtonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_action_button)
+    }
+}
