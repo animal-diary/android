16abb2ce1c2df737a799cf915c837069202b1853
Chamin Im <ckals413@naver.com>
2025-08-17 12:57:48 +0900

    :sparkles: [FEAT](#40) BottomNavigation 적용

--- app/src/main/java/com/example/animaldiary/MainActivity.kt
+++ app/src/main/java/com/example/animaldiary/MainActivity.kt
@@ -5,16 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
+        }
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_activity -> {
+                    // 화면 이동로직
+                    true
+                }
+                R.id.navigation_edit -> {
+
+                    true
+                }
+                R.id.navigation_settings -> {
+
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
