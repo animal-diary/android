b60de6a39ea5c45576bd4f0ae9ca37973fd93fcd
Chamin Im <ckals413@naver.com>
2025-08-27 16:50:27 +0900

    :sparkles: [FEAT](#57) 바텀시트 화면 연결

--- app/src/main/java/com/example/animaldiary/MainActivity.kt
+++ app/src/main/java/com/example/animaldiary/MainActivity.kt
@@ -5,9 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.animaldiary.ui.manage.ManageFragment
+import com.example.animaldiary.ui.setting.SettingFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private fun switchFragment(f: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, f)
+            .commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,18 +32,24 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        // 최초 진입 시 기본 화면 (관리 탭)
+        if (savedInstanceState == null) {
+            switchFragment(ManageFragment())
+            bottomNav.selectedItemId = R.id.navigation_manage
+        }
+
         bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.navigation_activity -> {
-                    // 화면 이동로직
+                R.id.navigation_health_note -> { // 건강 노트
+                    switchFragment(SettingFragment())
                     true
                 }
-                R.id.navigation_edit -> {
-
+                R.id.navigation_manage -> { // 관리하기
+                    switchFragment(ManageFragment())
                     true
                 }
-                R.id.navigation_settings -> {
-
+                R.id.navigation_setting -> { // 세팅
+                    switchFragment(SettingFragment())
                     true
                 }
                 else -> false
